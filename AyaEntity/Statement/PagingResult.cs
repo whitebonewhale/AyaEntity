625a91c795bd77ef36a7b4870c69d223cec46b9e
ZhengLei <zhengleiv@gmail.com>
2018-12-20 12:19:11 +0800

    分页函数返回内容补充（当前页码，每页数量），重命名分页参数PageSize=>RowSize

--- AyaEntity/Statement/PagingResult.cs
+++ AyaEntity/Statement/PagingResult.cs
@@ -7,7 +7,21 @@ namespace AyaEntity.SQLTools
 {
 	public class PagingResult<T>
 	{
-		public int Total { get; set; }
+        /// <summary>
+        /// 当前页码
+        /// </summary>
+        public  int PageIndex { get; set; }
+        /// <summary>
+        /// 数据总数
+        /// </summary>
+	    public int Total { get; set; }
+        /// <summary>
+        /// 每页数量
+        /// </summary>
+        public int RowSize { get; set; }
+        /// <summary>
+        /// 当前页码内容
+        /// </summary>
 		public IEnumerable<T> Rows { get; set; }
 	}
 }
